0333741ede890e1196cf19e0d15e5ddb7e1a3e6b
MutiaraCintaPratiwi <mutiaracinta939@gmail.com>
2025-12-28 15:59:47 +0700

    Add files via upload

---
+++ jsprofile.js
@@ -0,0 +1,8 @@
+function openYoutube() {
+    window.open("https://www.youtube.com", "_blank");
+}
+
+function downloadPDF() {
+    alert("Download PDF");
+    window.location.href = "assets/pdf/materi-jaringan.pdf";
+}
